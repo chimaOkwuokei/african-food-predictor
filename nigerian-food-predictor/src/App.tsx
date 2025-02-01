2d1f168d6a75bb667f151062a3210cf3af89252d
unknown <chima.okwuokei@pau.edu.ng>
2025-02-01 16:50:16 +0100

    refactor: removed footer

--- nigerian-food-predictor/src/App.tsx
+++ nigerian-food-predictor/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/home';
 import { Predict } from './pages/predict';
 import UserHeader from './components/header';
-import { Footer } from './components/footer';
+// import { Footer } from './components/footer';
 // import './App.css'
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
           <Route path="/predict-food" element={<Predict />} />
         </Routes>
       </Router>
-      <Footer/>
+      {/* <Footer/> */}
     </>
   )
 }
